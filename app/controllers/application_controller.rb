67d9ce8c58ce8eefac9f063d6f8865e1eb993b4b
quachhuytung <huytung.9729@gmail.com>
2019-08-14 09:47:05 +0700

    Refactor partial and using namespace controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  layout :choose_layout
+
+  private
+  def choose_layout 
+    if true
+      "users_layout"
+    else
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  scope module: :users do
+  namespace :users, path: "/" do 
     root to: 'home_pages#index'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
