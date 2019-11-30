45c3e07da17baa90bcc149afe0021efc95b6174c
dainguyenblabla <dainguyenblabla@gmail.com>
2019-11-30 22:48:08 +0700

    add giao dien datn

--- app/controllers/user_controllers/home_pages_controller.rb
+++ app/controllers/user_controllers/home_pages_controller.rb
@@ -1,4 +1,8 @@
 class UserControllers::HomePagesController < ApplicationController
   def index
   end
+
+  def show
+    @product= User.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :user_controllers, path: "/" do 
     root to: 'home_pages#index'
+    get  '/show',    to: 'home_pages#show'
     namespace :authentications do 
       resource :registrations, only: [:new, :create]
       resource :sessions, only: [:new, :create, :destroy]
